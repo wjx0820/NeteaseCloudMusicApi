0a4a367b1ebe0cf5dc946eb221448fd24dcc396e
Jexxie <1989wjx@gmail.com>
2020-07-22 22:13:20 +0800

    修改 ip 地址

--- util/request.js
+++ util/request.js
@@ -35,7 +35,7 @@ const chooseUserAgent = (ua) => {
 const createRequest = (method, url, data, options) => {
 	return new Promise((resolve, reject) => {
 		let headers = { 'User-Agent': chooseUserAgent(options.ua) }
-		headers['X-Real-IP'] = '211.161.244.70'
+		headers['X-Real-IP'] = '47.94.164.111'
 		if (method.toUpperCase() === 'POST')
 			headers['Content-Type'] = 'application/x-www-form-urlencoded'
 		if (url.includes('music.163.com'))
